496a7158571568f6bf49a8d98be94461ddc7d8ee
samsonmuoki <49044273+samsonmuoki@users.noreply.github.com>
2019-03-29 03:35:15 +0300

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -1,60 +1,78 @@
-<? php
-
-require_once('AfricasTalkingGateway.php')
-
-$username = "sandbox";
-$apikey   = "a41af90b1785c3cde4cda3fbdda5574c5dbc71d689ef66dd030143e92b5c0c95";
 
 
-//create table members  with columns phonenumber varchar(15), email varchar(50)
+#MUOKI SAMSON NGULI
 
-if(!empty($_POST)){
-	
-	require_once('AfricasTalkingGateway');
-	
-	
-	//receiving post from AT
-	$sessionId=$_POST['sessionId'];
-	$serviceCOde=$_POST['serviceCode'];
-	$phoneNumber=$_POST['phoneNumber'];
-	
-	$text=$_POST['text'];
-	
-//get value of latest interaction
-$textArray = explode('*',$text);
-
-$userResponse = trim(end($textArray));
-
-//check level
-$level = 0;
-$sql = "select 'level' from 'session_levels' where 'session_id'='"  . $sessionId . "'";
-$levelQuery = $db->query($sql);
-if($result = $levelQuery->fetch_assoc()){
-	$level = $result['level'];
+<?php
+// Reads the variables sent via POST from our gateway
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
+if ( $text == "" ) {
+   // This is the first request. Note how we start the response with CON
+   $response  = " CON Welcome to the youth forum  \n";
+   $response .= "Please enter your details to register";
+   
 }
 
-//check if user is not in db 
-$firstQuery="SELECT * FROM users WHERE 'phoNenumber' LIKE '%'"..$phoneNumber."%' LIMIT 1";
-$firstResult=$db->query($firstQuery);
-$userAvail=$firstResult->fetch_assoc();
+//explode session levels 
+$level = explode('*',$text);
 
-//print user details
-$response = "you have successfully registered";
-$response = $phoneNumber;
-$response = $email;
 
-//if user available reply user already registered
-if($userAvail && userAvail['email']!=NULL && $userAvail['phoneNumber']!=NULL ){
-	$response = "User ALREADY REGISTERED!";
+//if user enters username
+if(isset($level[0]) && $level[0]!=”” && !isset($level[1])){
+
+$response="CON ".$level[0].", enter your user name";
+ 
+ }
+ else if(isset($level[1]) && $level[1]!=”” && !isset($level[2])){
+ $response="CON Please enter your email\n";
+
+}
+
+else if(isset($level[1]) && $level[1]!="" && !isset($level[2])){
+            //Save data to  the database
+            $data=array(
+                'phonenumber'=>$phonenumber,
+                'username' =>$level[0],
+                'email' => $level[1],
+                
+                );
+
+ 
+// Print the response onto the page so that AT gateway can read it
+header('Content-type: text/plain');
+echo $response;
+
+
+// Specify the numbers that you want to send to in a comma-separated list
+// Please ensure you include the country code (+254 for Kenya in this case)
+$recipients = $phonenumber;
+
+
+// Send message to customer
+$message    = "You have successfully registered 'username=" . $username. ";" "email=" .$email. "";
+// Create a new instance of gateway class
+$gateway    = new AfricasTalkingGateway($username, $apikey);
+// Any gateway error will be captured by our custom Exception class below, 
+// so wrap the call in a try-catch block
+try 
+{ 
+  // Message is send to customer 
+  $results = $gateway->sendMessage($recipients, $message);
+			
+  foreach($results as $result) {
+    // status is either "Success" or "error message"
+    echo " Number: " .$result->number;
+    echo " Status: " .$result->status;
+    echo " MessageId: " .$result->messageId;
+    echo " Cost: "   .$result->cost."\n";
+  }
+}
+catch ( AfricasTalkingGatewayException $e )
+{
+  echo "Encountered an error while sending: ".$e->getMessage();
 }
 
 
-	
-	
-	
-	
-	
-}
-
-
-?>
+?> 
